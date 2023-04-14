1b6049392ad42b2c8c14ccbb87a9d064e2180b78
cyrus chen <pschen@lcs.on.ca>
2023-04-14 11:29:27 -0400

    Button now works to show the punchline.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct JokeView: View {
+    
+    @State var punchlineOpacity = 0.0
+    
     var body: some View {
+        
         NavigationView{
+            
         VStack{
+            
                 Text("You can see mountain's aren't funny.")
                 .font(.title)
                 .multilineTextAlignment(.center)
+            
             Button(action: {
-                
+                punchlineOpacity = 1.0
             }, label: {
                 Image(systemName: "arrow.down.circle.fill")
                     .resizable()
@@ -23,10 +30,14 @@ struct JokeView: View {
                     .frame(width: 40)
                     .tint(.black)
             })
+            
             Text("They are hill areas")
                 .font(.title)
                 .multilineTextAlignment(.center)
+                .opacity(punchlineOpacity)
+            
             }
+            
         .navigationTitle("Random Jokes")
         }
     }
