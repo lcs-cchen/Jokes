78cf00f44d54f8ccb4228e109a92a772285af056
cyrus chen <pschen@lcs.on.ca>
2023-04-14 11:13:51 -0400

    Re-organized to add View group.

---
+++ Jokes/Views/JokeView.swift
@@ -0,0 +1,20 @@
+//
+//  JokeView.swift
+//  Jokes
+//
+//  Created by Cyrus Chen on 14/4/2023.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeView()
+    }
+}
