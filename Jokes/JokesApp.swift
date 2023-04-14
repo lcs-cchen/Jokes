077bc1ff40ab06fb30ae72adfdf7903619e0545f
cyrus chen <pschen@lcs.on.ca>
2023-04-14 11:12:03 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Cyrus Chen on 14/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
